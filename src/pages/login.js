19b322e194dee3a1d2dbebcaabf68a8a9c4380de
TarunVerma200109 <verma.tarun200109@gmail.com>
2022-07-08 00:43:36 +0530

    login framework created

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,24 @@
 import React from "react";
+import styles from "./login.module.css";
 
 function Login() {
   return (
-    <div>
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button>Login</button>
+    <div className={styles.page}>
+      <div className={styles.form}>
+        <div className={styles.title}>
+          <h1>Hey missed you</h1>
+          <h1>Welcome back</h1>
+        </div>
+        <h3>Login</h3>
+        <input className={styles.email} type="text" placeholder="Email" />
+        <h3>Email</h3>
+        <input
+          className={styles.password}
+          type="password"
+          placeholder="Password"
+        />
+        <button className={styles.button}>Login</button>
+      </div>
     </div>
   );
 }
