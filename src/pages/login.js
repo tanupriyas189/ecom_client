46853925df72d01458437ef5f180c556340ea515
TarunVerma200109 <verma.tarun200109@gmail.com>
2022-07-08 01:15:36 +0530

    refractored

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./login.module.css";
 
 function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <div className={styles.page}>
       <div className={styles.form}>
@@ -10,14 +13,27 @@ function Login() {
           <h1>Welcome back</h1>
         </div>
         <h3>Login</h3>
-        <input className={styles.email} type="text" placeholder="Email" />
+        <input
+          onChange={(item) => setEmail(item.target.value)}
+          value={email}
+          className={styles.email}
+          type="text"
+          placeholder="Email"
+        />
         <h3>Email</h3>
         <input
+          onChange={(item) => setPassword(item.target.value)}
+          value={password}
           className={styles.password}
           type="password"
           placeholder="Password"
         />
-        <button className={styles.button}>Login</button>
+        <button
+          onClick={() => console.log(email, " ", password)}
+          className={styles.button}
+        >
+          Login
+        </button>
       </div>
     </div>
   );
